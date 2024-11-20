eaa310b836e0ddeb8c61cc3f57909f137d261797
jyash1 <your-email@example.com>
2024-11-20 14:55:13 +0530

    updated header reposiveness

--- src/components/main-header.js
+++ src/components/main-header.js
@@ -1,15 +1,17 @@
 // Header.js
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Box,} from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Box, } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const MainHeader = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#1abc9c' }}>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="h6" sx={{ fontWeight: 600 }}>
-          Spero Home Healthcare Services
-        </Typography>
+      <Toolbar sx={{ display: { md: 'flex' }, flexDirection: { md: "row", xs: "column" }, justifyContent: 'space-between' }}>
+        <Box>
+          <Typography variant="h6" sx={{ fontWeight: 600 }}>
+            Spero Home Healthcare Services
+          </Typography>
+        </Box>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
           <Typography variant="h6">Welcome, Management Dashboard</Typography>
           <IconButton>
